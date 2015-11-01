f0699b5ce051be3be1f7777fd477eead003b0737
bergi <bergi@axolotlfarm.org>
2015-11-01 22:14:57 +0100

    fixed code style errors

--- index.js
+++ index.js
@@ -1,106 +1,102 @@
-'use strict';
-
-//TODO: handle blank nodes
 var SparqlStore = function (rdf, options) {
-  var
-    self = this;
+  var self = this
 
-  options = options || {};
+  options = options || {}
 
-  self.endpointUrl = options.endpointUrl;
-  self.updateUrl = options.updateUrl || self.endpointUrl;
-  self.mimeType = options.mimeType || 'text/turtle';
-  self.serialize = options.serialize  || rdf.serializeNTriples;
-  self.parse = options.parse || rdf.parseTurtle;
-  self.request = options.request || rdf.defaultRequest;
+  self.endpointUrl = options.endpointUrl
+  self.updateUrl = options.updateUrl || self.endpointUrl
+  self.mimeType = options.mimeType || 'text/turtle'
+  self.serialize = options.serialize || rdf.serializeNTriples
+  self.parse = options.parse || rdf.parseTurtle
+  self.request = options.request || rdf.defaultRequest
 
   var httpSuccess = function (statusCode) {
-    return (statusCode >= 200 && statusCode < 300);
-  };
+    return (statusCode >= 200 && statusCode < 300)
+  }
 
   var buildMatch = function (subject, predicate, object) {
-    var match = '';
+    var match = ''
 
     var nodeToNT = function (node) {
       if (typeof node === 'string') {
         if (node.substr(0, 2) === '_:') {
-          return node;
+          return node
         } else {
-          return '<' + node + '>';
+          return '<' + node + '>'
         }
       }
 
-      return node.toNT();
-    };
+      return node.toNT()
+    }
 
-    match += subject ? nodeToNT(subject) : '?s';
-    match += predicate ? ' ' + nodeToNT(predicate) : ' ?p';
-    match += object ? ' ' + nodeToNT(object) : ' ?o';
+    match += subject ? nodeToNT(subject) : '?s'
+    match += predicate ? ' ' + nodeToNT(predicate) : ' ?p'
+    match += object ? ' ' + nodeToNT(object) : ' ?o'
 
-    return match;
-  };
+    return match
+  }
 
   self.executeQuery = function (queryString, callback, queryParams, requestOptions) {
-    queryParams = queryParams || {parse: true};
-    requestOptions = requestOptions || {};
+    queryParams = queryParams || {parse: true}
+    requestOptions = requestOptions || {}
 
-    var queryUrl = self.endpointUrl + '?query=' + encodeURIComponent(queryString);
-    var reqHeaders = { 'Accept': self.mimeType };
+    var queryUrl = self.endpointUrl + '?query=' + encodeURIComponent(queryString)
+    var reqHeaders = { 'Accept': self.mimeType }
 
     self.request('GET', queryUrl, reqHeaders, null, function (statusCode, resHeaders, resContent, error) {
       // error during request
       if (error) {
-        return callback('request error: ' + error);
+        return callback('request error: ' + error)
       }
 
       // http status code != success
       if (!httpSuccess(statusCode)) {
-        return callback('status code error: ' + statusCode);
+        return callback('status code error: ' + statusCode)
       }
 
       if (statusCode === 200 && queryParams.parse) {
-        self.parse(resContent, callback);
+        self.parse(resContent, callback)
       } else {
-        callback();
+        callback()
       }
-    });
-  };
+    })
+  }
 
   self.executeUpdateQuery = function (queryString, callback, queryParams, requestOptions) {
-    queryParams = queryParams || {parse: true};
-    requestOptions = requestOptions || {};
+    queryParams = queryParams || {parse: true}
+    requestOptions = requestOptions || {}
 
-    var reqHeaders = { 'Content-Type': 'application/sparql-update' };
+    var reqHeaders = { 'Content-Type': 'application/sparql-update' }
 
     self.request('POST', self.updateUrl, reqHeaders, queryString, function (statusCode, resHeaders, resContent, error) {
       // error during request
       if (error) {
-        return callback('request error: ' + error);
+        return callback('request error: ' + error)
       }
 
       // http status code != success
       if (!httpSuccess(statusCode)) {
-        return callback('status code error: ' + statusCode);
+        return callback('status code error: ' + statusCode)
       }
 
       if (statusCode === 200 && queryParams.parse) {
-        self.parse(resContent, callback);
+        self.parse(resContent, callback)
       } else {
-        callback();
+        callback()
       }
-    });
-  };
+    })
+  }
 
   self.graph = function (graphIri, callback) {
-    self.match(graphIri, null, null, null, callback);
-  };
+    self.match(graphIri, null, null, null, callback)
+  }
 
   self.match = function (graphIri, subject, predicate, object, callback, limit) {
-    var filter = buildMatch(subject, predicate, object);
-    var query = 'CONSTRUCT { ' + filter + ' } { GRAPH <' + graphIri + '> {' + filter + ' }}'; // TODO: use limit parameters
+    var filter = buildMatch(subject, predicate, object)
+    var query = 'CONSTRUCT { ' + filter + ' } { GRAPH <' + graphIri + '> {' + filter + ' }}'
 
-    self.executeQuery(query, callback);
-  };
+    self.executeQuery(query, callback)
+  }
 
   self.add = function (graphIri, graph, callback) {
     self.serialize(graph, function (error, data) {
@@ -109,60 +105,60 @@ var SparqlStore = function (rdf, options) {
       } else {
         var query = '' +
           'DROP SILENT GRAPH <' + graphIri + '>;' +
-          'INSERT DATA { GRAPH <' + graphIri + '> { ' + data + ' } }';
+          'INSERT DATA { GRAPH <' + graphIri + '> { ' + data + ' } }'
 
         self.executeUpdateQuery(query, function (error) {
           if (error) {
-            callback(error);
+            callback(error)
           } else {
-            callback(null, graph);
+            callback(null, graph)
           }
-        });
+        })
       }
-    });
-  };
+    })
+  }
 
   self.merge = function (graphIri, graph, callback) {
     self.serialize(graph, function (error, data) {
       if (error) {
         callback(error)
       } else {
-        var query = 'INSERT DATA { GRAPH <' + graphIri + '> { ' + data +' } }';
+        var query = 'INSERT DATA { GRAPH <' + graphIri + '> { ' + data + ' } }'
 
         self.executeUpdateQuery(query, function (error) {
           if (error) {
-            callback(error);
+            callback(error)
           } else {
-            callback(null, graph);
+            callback(null, graph)
           }
-        });
+        })
       }
-    });
-  };
+    })
+  }
 
   self.remove = function (graphIri, graph, callback) {
     self.serialize(graph, function (error, data) {
       if (error) {
         callback(error)
       } else {
-        var query = 'DELETE DATA FROM <' + graphIri + '> { ' + data + ' }';
+        var query = 'DELETE DATA FROM <' + graphIri + '> { ' + data + ' }'
 
-        self.executeUpdateQuery(query, callback);
+        self.executeUpdateQuery(query, callback)
       }
-    });
-  };
+    })
+  }
 
   self.removeMatches = function (graphIri, subject, predicate, object, callback) {
-    var query = 'DELETE FROM GRAPH <' + graphIri + '> WHERE { ' + buildMatch(subject, predicate, object) + ' }';
+    var query = 'DELETE FROM GRAPH <' + graphIri + '> WHERE { ' + buildMatch(subject, predicate, object) + ' }'
 
-    self.executeUpdateQuery(query, callback);
-  };
+    self.executeUpdateQuery(query, callback)
+  }
 
   self.delete = function (graphIri, callback) {
-    var query = 'CLEAR  GRAPH <' + graphIri + '>';
+    var query = 'CLEAR  GRAPH <' + graphIri + '>'
 
-    self.executeUpdateQuery(query, callback);
-  };
-};
+    self.executeUpdateQuery(query, callback)
+  }
+}
 
 module.exports = SparqlStore
